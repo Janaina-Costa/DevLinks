6585e720f6f0818ce856e55cf94dfd6e331719bc
Janaina-Costa <janainacosta.dev@gmail.com>
2023-07-29 17:50:35 -0300

    altera imagem do perfil

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ const toggleMode =()=>{
 
     btn.addEventListener('click', ()=>{
         page.classList.toggle('light')
-       if(page.classList.contains('light')){
-        img.setAttribute('src', './assets/jana.jpg')
-        img.setAttribute('alt', 'Imagem ilustrada de uma menina negra, fort e empoderada, com cableo cacheado preto e fundo amarelo')
-       }else{        
-            window.location.reload()
-            img.setAttribute('src', '"https://github.com/janaina-costa.png')
-            img.setAttribute('alt', 'Foto da Janaina sorrindo vestindo uma blusa preta')
-        
-       }
+       
     })
 }
 
